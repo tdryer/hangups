1d2b6cb5bec7bbc25599d096d1dc84dad3a93fbd
Tom Dryer <tomdryer.com@gmail.com>
2014-05-21 19:35:14 -0700

    Refactor code into ConversationWidget

--- hangups/__main__.py
+++ hangups/__main__.py
@@ -1,7 +1,7 @@
 """Demo chat client using Hangups."""
 
 from tornado import ioloop, gen
-import datetime
+from datetime import datetime
 import logging
 import time
 import urwid
@@ -10,11 +10,11 @@ from hangups.client import HangupsClient
 from hangups import auth
 
 
-# TODO: not using these styles yet
 URWID_PALETTE = [
     ('header', 'bold', 'default'),
-    ('msg_header', 'bold', 'default'),
-    ('msg', '', 'default'),
+    ('msg_date', 'dark blue', 'default'),
+    ('msg_sender', 'dark blue', 'default'),
+    ('msg_text', '', 'default'),
 ]
 
 
@@ -25,12 +25,12 @@ class DemoClient(HangupsClient):
         super().__init__()
 
         self._urwid_loop = urwid_loop
-        # method for adding lines to the conversation view
-        self.add_line = None
+        self.conv_widget = None
 
         # ID of the conversation to listen to
         self.conversation_id = None
         self.contacts = None
+        self.conversations = None
 
     def get_contact_name(self, user_ids):
         """Return the name of a contact."""
@@ -42,10 +42,11 @@ class DemoClient(HangupsClient):
         self.conversation_id = conv_id
 
         # show the conversation view
-        widget, self.add_line = conversation_view(
-            'TODO conversation name here', self.on_send_message
-        )
-        self._urwid_loop.widget = widget
+        participants_dict = {user_ids: self.contacts[user_ids] for user_ids
+                             in self.conversations[conv_id]['participants']}
+        self.conv_widget = ConversationWidget(participants_dict,
+                                              self.on_send_message)
+        self._urwid_loop.widget = self.conv_widget
 
     @gen.coroutine
     def on_send_message(self, text):
@@ -55,6 +56,7 @@ class DemoClient(HangupsClient):
     @gen.coroutine
     def on_connect(self, conversations, contacts):
         self.contacts = contacts
+        self.conversations = conversations
 
         # build dict of conv ID -> conv name
         conv_dict = {}
@@ -73,14 +75,8 @@ class DemoClient(HangupsClient):
     def on_message_receive(self, conversation_id, message):
         if conversation_id == self.conversation_id:
             user_ids = (message.user_chat_id, message.user_gaia_id)
-            self.add_line(
-                '({}) {}: {}'.format(
-                    datetime.datetime.fromtimestamp(
-                        message.timestamp / 1000000
-                    ).strftime('%I:%M:%S %p'), self.get_contact_name(user_ids),
-                    message.text
-                )
-            )
+            self.conv_widget.display_message(message)
+
             # respond to a \time message with the unix time
             if message.text == '\\time':
                 yield self.send_message(
@@ -93,18 +89,12 @@ class DemoClient(HangupsClient):
     def on_focus_update(self, conversation_id, user_ids, focus_status,
                         focus_device):
         if conversation_id == self.conversation_id:
-            self.add_line(
-                '{} {} the conversation on {}'
-                .format(self.get_contact_name(user_ids), focus_status,
-                        focus_device)
-            )
+            pass # TODO implement displaying this in the ConversationWidget
 
     @gen.coroutine
     def on_typing_update(self, conversation_id, user_ids, typing_status):
         if conversation_id == self.conversation_id:
-            self.add_line(
-                '{} {}'.format(self.get_contact_name(user_ids), typing_status)
-            )
+            pass # TODO implement displaying this in the ConversationWidget
 
     @gen.coroutine
     def on_disconnect(self):
@@ -141,26 +131,49 @@ class ReturnableEdit(urwid.Edit):
         else:
             return key
 
+class ConversationWidget(urwid.WidgetWrap):
+    """Widget for interacting with a conversation."""
 
-def conversation_view(conv_name, on_send_message_coroutine):
-    """Return an urwid widget for showing a conversation."""
-    def on_return(text):
-        future = on_send_message_coroutine(text)
+    def __init__(self, participants_dict, send_message_coroutine):
+        self._participants = participants_dict
+        self._send_message_coroutine = send_message_coroutine
+
+        names_str = ', '.join(p['first_name']
+                              for p in participants_dict.values())
+        title = 'Conversation: {}'.format(names_str)
+        self._list_walker = urwid.SimpleFocusListWalker([])
+        self._list_box = urwid.ListBox(self._list_walker)
+        self._widget = urwid.Pile([
+            ('pack', urwid.Text(('header', title), align='center')),
+            ('weight', 1, self._list_box),
+            ('pack', ReturnableEdit(self._on_return, caption='Send message: ')),
+        ])
+        # focus the edit widget by default
+        self._widget.focus_position = 2
+        super().__init__(self._widget)
+
+    def _on_return(self, text):
+        """Called when the user presses return on the send message widget."""
+        future = self._send_message_coroutine(text)
         ioloop.IOLoop.instance().add_future(future, lambda f: f.result())
-    line_list = urwid.SimpleFocusListWalker([])
-    list_box = urwid.ListBox(line_list)
-    def add_line(line):
-        line_list.append(urwid.Text(line))
-        list_box.set_focus(len(line_list) - 1)
-    widget = urwid.Pile([
-        ('pack', urwid.Text(('header', 'Conversation: {}'
-                             .format(conv_name)), align='center')),
-        ('weight', 1, list_box),
-        ('pack', ReturnableEdit(on_return, caption='Send message: ')),
-    ])
-    # focus the Edit widget by default
-    widget.focus_position = 2
-    return (widget, add_line)
+
+    def display_message(self, message):
+        """Display a new conversation message."""
+        # format the message
+        date_int = message.timestamp / 1000000
+        date_str = datetime.fromtimestamp(date_int).strftime('%I:%M:%S %p')
+        user_ids = (message.user_chat_id, message.user_gaia_id)
+        name = self._participants[user_ids]['first_name']
+
+        # add the message to the list box
+        self._list_walker.append(urwid.Text([
+            ('msg_date', '(' + date_str + ') '),
+            ('msg_sender', name + ': '),
+            ('msg_text', message.text)
+        ]))
+
+        # scroll down to the new message
+        self._list_box.set_focus(len(self._list_walker) - 1)
 
 
 @gen.coroutine
