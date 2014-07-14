155e5f7d560ff2608418f0a3128d866fa9eb7ffc
Tom Dryer <tomdryer.com@gmail.com>
2014-07-14 12:23:37 -0700

    Replace notify-send with gdbus for notifications
    This lets us instantly update notifications when additional messages
    arrive.

--- hangups/notify.py
+++ hangups/notify.py
@@ -1,27 +1,39 @@
 """Graphical notifications for the hangups UI.
 
 TODO:
-    - Support different notification systems either by allowing NOTIFY_CMD to
-    be customized, or by implementing Notifier subclasses.
-    - Replace notify-send with something that supports merging notifications.
+    - Support other notification systems.
     - Create notifications for other events like (dis)connection
 """
 
+import html
 import logging
+import re
 import subprocess
 
 logger = logging.getLogger(__name__)
-NOTIFY_CMD = ['notify-send', '{sender_name}', '{msg_text}']
+NOTIFY_CMD = [
+    'gdbus', 'call', '--session', '--dest', 'org.freedesktop.Notifications',
+    '--object-path', '/org/freedesktop/Notifications', '--method',
+    'org.freedesktop.Notifications.Notify', 'hangups', '{replaces_id}', '',
+    '{sender_name}', '{msg_text}', '[]', '{{}}', ' -1'
+]
+RESULT_RE = re.compile(r'\(uint32 ([\d]+),\)')
 
 
 class Notifier(object):
 
-    """Receives events from hangups and creates system notifications."""
+    """Receives events from hangups and creates system notifications.
+
+    This uses the gdbus utility to create freedesktop.org notifications. If a
+    new notification is created while a previous one is still open, the
+    previous notification is instantly replaced.
+    """
 
     def __init__(self, client, conv_list):
         self._conv_list = conv_list  # hangups.ConversationList
         self._client = client  # hangups.Client
         self._client.on_message += self._on_message
+        self._replaces_id = 0
 
     def _on_message(self, client, conv_id, user_id, timestamp, text):
         """Create notification for new messages."""
@@ -29,12 +41,25 @@ class Notifier(object):
         user = conv.get_user(user_id)
         # Ignore messages sent by yourself.
         if not user.is_self:
+            # We have to escape angle brackets because freedesktop.org
+            # notifications support markup.
             cmd = [arg.format(
-                sender_name=user.full_name,
-                msg_text=text,
+                sender_name=html.escape(user.full_name, quote=False),
+                msg_text=html.escape(text, quote=False),
+                replaces_id=self._replaces_id,
             ) for arg in NOTIFY_CMD]
-            logger.info('Creating notification with command: {}'.format(cmd))
-            # Run the command without blocking, and without a shell. Since the
+
+            # Run the notification and parse out the replaces_id. Since the
             # command is a list of arguments, and we're not using a shell, this
             # should be safe.
-            subprocess.Popen(cmd)
+            logger.info('Creating notification with command: {}'.format(cmd))
+            try:
+                output = subprocess.check_output(cmd).decode()
+            except (subprocess.CalledProcessError, FileNotFoundError) as e:
+                logger.warning('Notification command failed: {}'.format(e))
+                return
+            try:
+                self._replaces_id = RESULT_RE.match(output).groups()[0]
+            except (AttributeError, IndexError) as e:
+                logger.warning('Failed to parse notification command '
+                               'result: {}'.format(e))
