54c87c78eeb56dc580f2c8e07c9e077cdf42bd3a
Tom Dryer <tomdryer.com@gmail.com>
2014-09-30 19:27:48 -0700

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.rst') as f:
 
 setup(
     name='hangups',
-    version='0.2.0',
+    version='0.2.1',
     description=('the first third-party instant messaging client for Google '
                  'Hangouts'),
     long_description=readme,
