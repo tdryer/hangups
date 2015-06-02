8e07b69bd36a8f7c0f2a433a6a6e520d2996dd88
Francisco Pina Martins <f.pinamartins@gmail.com>
2015-06-02 12:19:02 +0100

    Correted a bug in a conditional.

--- hangups/ui/__main__.py
+++ hangups/ui/__main__.py
@@ -789,7 +789,7 @@ def set_terminal_title(title):
 def dir_maker(path):
     """Create a directory if it does not exist."""
     directory = os.path.dirname(path)
-    if directory != '' and not os.path.isdir(directory):
+    if directory != '' and if not os.path.isdir(directory):
         try:
             os.makedirs(directory)
         except OSError as e:
