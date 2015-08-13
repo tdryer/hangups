9fcb6c3c909e8f00a47de3a5fd25862547aa9ce1
Lana Larsen <lana@stoatly.com>
2015-08-13 04:38:12 -0700

    Fixed enum34 requirement issue per https://github.com/tdryer/hangups/issues/147

--- setup.py
+++ setup.py
@@ -23,6 +23,20 @@ class PyTest(TestCommand):
 with open('README.rst') as f:
     readme = f.read()
 
+install_requires=[
+    'ConfigArgParse==0.9.3',
+    'aiohttp==0.15.1',
+    'appdirs==1.4.0',
+    'purplex==0.2.4',
+    'readlike>=0.1',
+    'requests==2.6.0',
+    'ReParser>=1.4',
+    # use forked urwid until there's a 1.3 release with colour bugfix
+    'hangups-urwid==1.2.2-dev',
+]
+if sys.version_info < (3, 4):
+    install_requires.append('enum34=1.0.4')
+    install_requires.append('asyncio==3.4.3')
 
 setup(
     name='hangups',
@@ -44,21 +58,7 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
     packages=['hangups', 'hangups.ui'],
-    install_requires=[
-        'ConfigArgParse==0.9.3',
-        'aiohttp==0.15.1',
-        'appdirs==1.4.0',
-        'purplex==0.2.4',
-        'readlike>=0.1',
-        'requests==2.6.0',
-        'ReParser>=1.4',
-        # use forked urwid until there's a 1.3 release with colour bugfix
-        'hangups-urwid==1.2.2-dev',
-        # backport enum for python3.3:
-        'enum34==1.0.4',
-        # backport asyncio for python3.3:
-        'asyncio==3.4.3',
-    ],
+    install_requires=install_requires,
     tests_require=[
         'pytest',
     ],
