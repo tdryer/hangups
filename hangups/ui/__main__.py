30438d3a2051accca78c58d261e7988e791f90ed
Lana Larsen <lana@stoatly.com>
2015-08-13 05:19:39 -0700

    Change UI to use full names instead of just first names

--- hangups/ui/__main__.py
+++ hangups/ui/__main__.py
@@ -384,7 +384,7 @@ class StatusLineWidget(urwid.WidgetWrap):
 
     def _update(self):
         """Update status text."""
-        typers = [self._conversation.get_user(user_id).first_name
+        typers = [self._conversation.get_user(user_id).full_name
                   for user_id, status in self._typing_statuses.items()
                   if status == hangups.TypingStatus.TYPING]
         if len(typers) > 0:
@@ -416,7 +416,7 @@ class MessageWidget(urwid.WidgetWrap):
             ('msg_text', text)
         ]
         if user is not None:
-            text.insert(1, ('msg_sender', user.first_name + ': '))
+            text.insert(1, ('msg_sender', user.full_name + ': '))
         self._widget = urwid.Text(text)
         super().__init__(self._widget)
 
@@ -450,10 +450,10 @@ class MessageWidget(urwid.WidgetWrap):
         elif isinstance(conv_event, hangups.RenameEvent):
             if conv_event.new_name == '':
                 text = ('{} cleared the conversation name'
-                        .format(user.first_name))
+                        .format(user.full_name))
             else:
                 text = ('{} renamed the conversation to {}'
-                        .format(user.first_name, conv_event.new_name))
+                        .format(user.full_name, conv_event.new_name))
             return MessageWidget(conv_event.timestamp, text,
                                  show_date=is_new_day)
         elif isinstance(conv_event, hangups.MembershipChangeEvent):
@@ -462,7 +462,7 @@ class MessageWidget(urwid.WidgetWrap):
             names = ', '.join([user.full_name for user in event_users])
             if conv_event.type_ == hangups.MembershipChangeType.JOIN:
                 text = ('{} added {} to the conversation'
-                        .format(user.first_name, names))
+                        .format(user.full_name, names))
             else:  # LEAVE
                 text = ('{} left the conversation'.format(names))
             return MessageWidget(conv_event.timestamp, text,
