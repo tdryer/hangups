ec5b416e9e5b29a058f135c2c14a56968934e623
Tom Dryer <tomdryer.com@gmail.com>
2015-08-22 17:35:32 -0700

    Add comment about proto file imports in __init__

--- hangups/__init__.py
+++ hangups/__init__.py
@@ -1,9 +1,4 @@
 from .version import __version__
-# TODO just import *?
-from .hangouts_pb2 import (
-    TYPING_TYPE_STARTED, TYPING_TYPE_PAUSED, TYPING_TYPE_STOPPED,
-    MEMBERSHIP_CHANGE_TYPE_LEAVE, MEMBERSHIP_CHANGE_TYPE_JOIN
-)
 from .client import Client
 from .user import UserList, build_user_list
 from .conversation import ConversationList
@@ -12,3 +7,9 @@ from .exceptions import HangupsError, NetworkError
 from .conversation_event import (ChatMessageSegment, ConversationEvent,
                                  ChatMessageEvent, RenameEvent,
                                  MembershipChangeEvent)
+# Only import enum values and messages from the proto file that are necessary
+# for third-party code to use hangups.
+from .hangouts_pb2 import (
+    TYPING_TYPE_STARTED, TYPING_TYPE_PAUSED, TYPING_TYPE_STOPPED,
+    MEMBERSHIP_CHANGE_TYPE_LEAVE, MEMBERSHIP_CHANGE_TYPE_JOIN
+)
