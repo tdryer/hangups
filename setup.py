968e6fc7326ccc604a27d343207afdc64a92df30
Tom Dryer <tomdryer.com@gmail.com>
2015-08-26 20:20:45 -0700

    Only install backported libraries when required
    Using an out-of-date backported library with a newer version of Python
    can break things. See #147.

--- setup.py
+++ setup.py
@@ -24,6 +24,32 @@ with open('README.rst') as f:
     readme = f.read()
 
 
+install_requires = [
+    'ConfigArgParse==0.9.3',
+    'aiohttp==0.15.1',
+    'appdirs==1.4.0',
+    'purplex==0.2.4',
+    'readlike>=0.1',
+    'requests==2.6.0',
+    'ReParser>=1.4',
+    # use alpha protobuf for official Python 3 support
+    'protobuf==3.0.0a3',
+    # use forked urwid until there's a 1.3 release with colour bugfix
+    'hangups-urwid==1.2.2-dev',
+]
+
+
+if sys.version_info < (3, 4):
+    # For Python earlier than 3.4, a backport of enum is required.
+    install_requires.append('enum34==1.0.4')
+
+
+if sys.version_info < (3, 4, 3):
+    # For Python earlier than 3.4.3, use a backported asyncio that fixes an
+    # issue with an exception being logged on exit.
+    install_requires.append('asyncio==3.4.3')
+
+
 setup(
     name='hangups',
     version=__version__,
@@ -44,23 +70,7 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
     packages=['hangups', 'hangups.ui'],
-    install_requires=[
-        'ConfigArgParse==0.9.3',
-        'aiohttp==0.15.1',
-        'appdirs==1.4.0',
-        'purplex==0.2.4',
-        'readlike>=0.1',
-        'requests==2.6.0',
-        'ReParser>=1.4',
-        # use alpha protobuf for official Python 3 support
-        'protobuf==3.0.0a3',
-        # use forked urwid until there's a 1.3 release with colour bugfix
-        'hangups-urwid==1.2.2-dev',
-        # backport enum for python3.3:
-        'enum34==1.0.4',
-        # backport asyncio for python3.3:
-        'asyncio==3.4.3',
-    ],
+    install_requires=install_requires,
     tests_require=[
         'pytest',
     ],
