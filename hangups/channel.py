7c098d7b0de8257cca4f3a7269587ca4f5d38ff8
Tom Dryer <tomdryer.com@gmail.com>
2015-09-24 19:38:13 -0700

    Fix unknown SID error when requesting new SID

--- hangups/channel.py
+++ hangups/channel.py
@@ -262,14 +262,19 @@ class Channel(object):
     def _fetch_channel_sid(self):
         """Creates a new channel for receiving push data.
 
+        Sending an empty forward channel request will create a new channel on
+        the server.
+
+        There's a separate API to get the gsessionid alone that Hangouts for
+        Chrome uses, but if we don't send a gsessionid with this request, it
+        will return a gsessionid as well as the SID.
+
         Raises hangups.NetworkError if the channel can not be created.
         """
-        # Sending an empty forward channel request will create a new channel on
-        # the server.
         logger.info('Requesting new gsessionid and SID...')
-        # There's a separate API to get the gsessionid alone that Hangouts for
-        # Chrome uses, but if we don't send a gsessionid with this request, it
-        # will return a gsessionid as well as the SID.
+        # Set SID and gsessionid to None so they aren't sent in by send_maps.
+        self._sid_param = None
+        self._gsessionid_param = None
         res = yield from self.send_maps([])
         self._sid_param, self._gsessionid_param = _parse_sid_response(res.body)
         logger.info('New SID: {}'.format(self._sid_param))
