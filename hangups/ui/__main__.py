a8b2d3f67424c36020e84244760430096caf0b8e
Tom Dryer <tomdryer.com@gmail.com>
2016-07-23 15:21:21 -0700

    Fix hang in non-UTF-8 locales
    Prior to this fix, hangups would hang if a conversation is opened that
    contains a non-ASCII character in its name and the current locale isn't
    UTF-8.
    
    Fixes #249.

--- hangups/ui/__main__.py
+++ hangups/ui/__main__.py
@@ -805,8 +805,8 @@ class TabbedWindowWidget(urwid.WidgetWrap):
             palette = ('active_tab' if num == self._tab_index
                        else 'inactive_tab')
             text += [
-                (palette, ' {} '.format(self._widget_title[widget]).encode()),
-                ('tab_background', b' '),
+                (palette, ' {} '.format(self._widget_title[widget])),
+                ('tab_background', ' '),
             ]
         self._tabs.set_text(text)
         self._frame.contents['body'] = (self._widgets[self._tab_index], None)
