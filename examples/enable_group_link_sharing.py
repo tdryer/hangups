6b4aaf38b63a614d625dca9db53196c32fb5d59f
Tom Dryer <tomdryer.com@gmail.com>
2016-11-12 21:07:44 -0800

    Add group link sharing example

---
+++ examples/enable_group_link_sharing.py
@@ -0,0 +1,40 @@
+"""Example of using hangups to enable group link sharing in a conversation."""
+
+import asyncio
+
+import hangups
+
+from common import run_example
+
+
+@asyncio.coroutine
+def enable_group_link_sharing(client, args):
+    request = hangups.hangouts_pb2.SetGroupLinkSharingEnabledRequest(
+        request_header=client.get_request_header(),
+        event_request_header=hangups.hangouts_pb2.EventRequestHeader(
+            conversation_id=hangups.hangouts_pb2.ConversationId(
+                id=args.conversation_id
+            ),
+            client_generated_id=client.get_client_generated_id(),
+        ),
+        group_link_sharing_status=(
+            hangups.hangouts_pb2.GROUP_LINK_SHARING_STATUS_ON
+        ),
+    )
+    yield from client.set_group_link_sharing_enabled(request)
+    print('enabled group link sharing for conversation {}'.format(
+        args.conversation_id
+    ))
+
+    request = hangups.hangouts_pb2.GetGroupConversationUrlRequest(
+        request_header=client.get_request_header(),
+        conversation_id=hangups.hangouts_pb2.ConversationId(
+            id=args.conversation_id,
+        )
+    )
+    response = yield from client.get_group_conversation_url(request)
+    print(response.group_conversation_url)
+
+
+if __name__ == '__main__':
+    run_example(enable_group_link_sharing, '--conversation-id')
