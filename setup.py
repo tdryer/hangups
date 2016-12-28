803630e48b836b45087e22bec315221ac75ec0af
Tom Dryer <tomdryer.com@gmail.com>
2016-12-28 13:33:21 -0800

    Add dependency version range for requests

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ install_requires = [
     'aiohttp>=1.2,<1.3',
     'appdirs==1.4.0',
     'readlike==0.1.2',
-    'requests==2.6.0',
+    'requests>=2.6.0,<3',  # uses semantic versioning (after 2.6)
     'ReParser==1.4.3',
     'protobuf>=3.1.0,<3.2.0',
     'urwid==1.3.1',
