e5b4e5a811f74b0ac8ff10229563a6475b38a237
Tom Dryer <tomdryer.com@gmail.com>
2017-02-16 20:52:25 -0800

    Add set_focus example

---
+++ examples/set_focus.py
@@ -0,0 +1,24 @@
+"""Example of using hangups to set focus to a conversation."""
+
+import asyncio
+
+import hangups
+
+from common import run_example
+
+
+@asyncio.coroutine
+def set_focus(client, args):
+    request = hangups.hangouts_pb2.SetFocusRequest(
+        request_header=client.get_request_header(),
+        conversation_id=hangups.hangouts_pb2.ConversationId(
+            id=args.conversation_id
+        ),
+        type=hangups.hangouts_pb2.FOCUS_TYPE_FOCUSED,
+        timeout_secs=int(args.timeout_secs),
+    )
+    yield from client.set_focus(request)
+
+
+if __name__ == '__main__':
+    run_example(set_focus, '--conversation-id', '--timeout-secs')
