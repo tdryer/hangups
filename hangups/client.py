5340d08c17690c5ba701456d2e214ae4d390fbf9
Tom Dryer <tomdryer.com@gmail.com>
2017-03-06 20:09:57 -0800

    Fix presence notification messages
    Add additional service that's now required to receive presence
    notification messages.
    
    See #196.

--- hangups/client.py
+++ hangups/client.py
@@ -318,8 +318,12 @@ class Client(object):
         """Add services to the channel.
 
         The services we add to the channel determine what kind of data we will
-        receive on it. The "babel" service includes what we need for Hangouts.
-        If this fails for some reason, hangups will never receive any events.
+        receive on it.
+
+        The "babel" service includes what we need for Hangouts. If this fails
+        for some reason, hangups will never receive any events. The
+        "babel_presence_last_seen" service is also required to receive presence
+        notifications.
 
         This needs to be re-called whenever we open a new channel (when there's
         a new SID and client_id.
@@ -328,7 +332,11 @@ class Client(object):
         # Based on what Hangouts for Chrome does over 2 requests, this is
         # trimmed down to 1 request that includes the bare minimum to make
         # things work.
-        map_list = [dict(p=json.dumps({"3": {"1": {"1": "babel"}}}))]
+        services = ["babel", "babel_presence_last_seen"]
+        map_list = [
+            dict(p=json.dumps({"3": {"1": {"1": service}}}))
+            for service in services
+        ]
         yield from self._channel.send_maps(map_list)
         logger.info('Channel services added')
 
