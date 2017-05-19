589d56dc05040d682ba3576916c1f818ec259894
Tom Dryer <tomdryer.com@gmail.com>
2017-05-18 20:00:33 -0700

    Add get_conversation example

---
+++ examples/get_conversation.py
@@ -0,0 +1,27 @@
+"""Example of using hangups to get conversation messages."""
+
+import asyncio
+
+import hangups
+
+from common import run_example
+
+
+@asyncio.coroutine
+def get_conversation(client, args):
+    request = hangups.hangouts_pb2.GetConversationRequest(
+        request_header=client.get_request_header(),
+        conversation_spec=hangups.hangouts_pb2.ConversationSpec(
+            conversation_id=hangups.hangouts_pb2.ConversationId(
+                id=args.conversation_id
+            ),
+        ),
+        include_event=True,
+        max_events_per_conversation=10,
+    )
+    res = yield from client.get_conversation(request)
+    print(res)
+
+
+if __name__ == '__main__':
+    run_example(get_conversation, '--conversation-id')
