d80a0320763195f74c69e0d6af1e429b94014ceb
Tom Dryer <tomdryer.com@gmail.com>
2017-06-04 11:49:21 -0700

    Add query_presence example

---
+++ examples/query_presence.py
@@ -0,0 +1,30 @@
+"""Example of using hangups to query presence of a user."""
+
+import asyncio
+
+import hangups
+
+from common import run_example
+
+
+@asyncio.coroutine
+def query_presence(client, args):
+    request = hangups.hangouts_pb2.QueryPresenceRequest(
+        request_header=client.get_request_header(),
+        participant_id=[
+            hangups.hangouts_pb2.ParticipantId(gaia_id=args.user_id),
+        ],
+        field_mask=[
+            hangups.hangouts_pb2.FIELD_MASK_REACHABLE,
+            hangups.hangouts_pb2.FIELD_MASK_AVAILABLE,
+            hangups.hangouts_pb2.FIELD_MASK_MOOD,
+            hangups.hangouts_pb2.FIELD_MASK_DEVICE,
+            hangups.hangouts_pb2.FIELD_MASK_LAST_SEEN,
+        ],
+    )
+    res = yield from client.query_presence(request)
+    print(res)
+
+
+if __name__ == '__main__':
+    run_example(query_presence, '--user-id')
