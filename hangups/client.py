34cff597f03e53e1dcbaf13b91b0a134437b2a90
das7pad <das7pad@outlook.com>
2017-08-09 12:51:01 +0200

    keep the ClientSession alive until the Client is garbage collected

--- hangups/client.py
+++ hangups/client.py
@@ -111,7 +111,8 @@ class Client(object):
 
     def __del__(self):
         """explicit cleanup"""
-        self._connector.close()
+        if self._session is not None:
+            self._session.close()
 
     ##########################################################################
     # Public methods
@@ -125,30 +126,26 @@ class Client(object):
         called.
         """
         self._session = aiohttp.ClientSession(cookies=self._cookies)
-        try:
-            self._channel = channel.Channel(
-                self._cookies, self._session, max_retries=self._max_retries,
-                retry_backoff_base=self._retry_backoff_base
-            )
-            # Forward the Channel events to the Client events.
-            self._channel.on_connect.add_observer(self.on_connect.fire)
-            self._channel.on_reconnect.add_observer(self.on_reconnect.fire)
-            self._channel.on_disconnect.add_observer(self.on_disconnect.fire)
-            self._channel.on_receive_array.add_observer(self._on_receive_array)
 
-            # Wrap the coroutine in a Future so it can be cancelled.
-            self._listen_future = asyncio.async(self._channel.listen())
-            # Listen for StateUpdate messages from the Channel until it
-            # disconnects.
-            try:
-                yield from self._listen_future
-            except asyncio.CancelledError:
-                pass
-            logger.info(
-                'Client.connect returning because Channel.listen returned'
-            )
-        finally:
-            self._session.close()
+        self._channel = channel.Channel(
+            self._cookies, self._session, max_retries=self._max_retries,
+            retry_backoff_base=self._retry_backoff_base
+        )
+        # Forward the Channel events to the Client events.
+        self._channel.on_connect.add_observer(self.on_connect.fire)
+        self._channel.on_reconnect.add_observer(self.on_reconnect.fire)
+        self._channel.on_disconnect.add_observer(self.on_disconnect.fire)
+        self._channel.on_receive_array.add_observer(self._on_receive_array)
+
+        # Wrap the coroutine in a Future so it can be cancelled.
+        self._listen_future = asyncio.async(self._channel.listen())
+        # Listen for StateUpdate messages from the Channel until it
+        # disconnects.
+        try:
+            yield from self._listen_future
+        except asyncio.CancelledError:
+            pass
+        logger.info('Client.connect returning because Channel.listen returned')
 
     @asyncio.coroutine
     def disconnect(self):
