fc95bda16711219600f3dbc74fb729ab32208223
das7pad <das7pad@outlook.com>
2017-09-23 22:12:12 +0200

    [message_parser] update the URL parsing
    the regex may match only urls with the `http` or `https` protocoll or no protocoll

--- hangups/message_parser.py
+++ hangups/message_parser.py
@@ -24,15 +24,31 @@ html_newline = r'(?i)<br\s*/?>'
 newline = r'\n|\r\n'
 
 # Based on URL regex pattern by John Gruber
-# (http://gist.github.com/gruber/249502)
-auto_link = (
-    r'(?i)\b('
-    r'(?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|'
-    r'[a-z0-9.\-]+[.][a-z]{2,4}/)'
-    r'(?:[^\s()<>]|\((?:[^\s()<>]|(?:\([^\s()<>]+\)))*\))+'
-    r'(?:\((?:[^\s()<>]|(?:\([^\s()<>]+\)))*\)|'
-    r'[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'
-)
+# (https://gist.github.com/gruber/8891611)
+_DOMAINS = (
+    'com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|'
+    'name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|'
+    'au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|'
+    'cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|'
+    'dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|'
+    'gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|'
+    'is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|'
+    'lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|'
+    'my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|'
+    'pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|'
+    'sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|'
+    'tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw')
+_BALANCED_PARENS = r'\([^\s()]*?\([^\s()]+\)[^\s()]*?\)'
+AUTO_LINK = (
+    (r'(?i)\b('
+     r'(?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:domains)/)'
+     r'(?:[^\s()<>{}\[\]]+|balanced_parens|\([^\s]+?\))+'
+     r'(?:balanced_parens|\([^\s]+?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])|'
+     r'(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:domains)\b/?(?!@)))')
+    .replace('domains', _DOMAINS).replace('balanced_parens', _BALANCED_PARENS))
+# cleanup
+del _DOMAINS
+del _BALANCED_PARENS
 
 # Precompiled regex for matching protocol part of URL
 url_proto_regex = re.compile(r'(?i)^[a-z][\w-]+:/{1,3}')
@@ -59,7 +75,7 @@ def url_complete(url):
 class Tokens:
     """Groups of tokens to be used by ChatMessageParser"""
     basic = [
-        Token('link', auto_link, link_target=MatchGroup('start',
+        Token('link', AUTO_LINK, link_target=MatchGroup('start',
                                                         func=url_complete)),
         Token('br', newline, text='\n',
               segment_type=hangouts_pb2.SEGMENT_TYPE_LINE_BREAK)
