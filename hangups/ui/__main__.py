ede7ec95d4e4195a4a56e687d69d009bbbfe252b
Tom Dryer <tomdryer.com@gmail.com>
2018-01-05 18:37:16 -0800

    Fix attempt to notify for non-message events

--- hangups/ui/__main__.py
+++ hangups/ui/__main__.py
@@ -190,12 +190,6 @@ class ChatUI(object):
         """Open conversation tab for new messages & pass events to notifier."""
         conv = self._conv_list.get(conv_event.conversation_id)
         user = conv.get_user(conv_event.user_id)
-        if self._discreet_notifications:
-            notification = DISCREET_NOTIFICATION
-        else:
-            notification = notifier.Notification(
-                user.full_name, get_conv_name(conv), conv_event.text
-            )
         show_notification = all((
             isinstance(conv_event, hangups.ChatMessageEvent),
             not user.is_self,
@@ -203,6 +197,12 @@ class ChatUI(object):
         ))
         if show_notification:
             self.add_conversation_tab(conv_event.conversation_id)
+            if self._discreet_notifications:
+                notification = DISCREET_NOTIFICATION
+            else:
+                notification = notifier.Notification(
+                    user.full_name, get_conv_name(conv), conv_event.text
+                )
             self._notifier.send(notification)
 
     def _on_quit(self):
