4d38d9325f69dd4dd6a5874f0cb85ff41bbc24d3
Tom Dryer <tomdryer.com@gmail.com>
2018-01-06 20:55:16 -0800

    Add example for receiving chat messages

---
+++ examples/receive_messages.py
@@ -0,0 +1,32 @@
+"""Example of using hangups to receive chat messages.
+
+Uses the high-level hangups API.
+"""
+
+import asyncio
+
+import hangups
+
+from common import run_example
+
+
+@asyncio.coroutine
+def receive_messages(client, args):
+    print('loading conversation list...')
+    user_list, conv_list = (
+        yield from hangups.build_user_conversation_list(client)
+    )
+    conv_list.on_event.add_observer(on_event)
+
+    print('waiting for chat messages...')
+    while True:
+        yield from asyncio.sleep(1)
+
+
+def on_event(conv_event):
+    if isinstance(conv_event, hangups.ChatMessageEvent):
+        print('received chat message: {!r}'.format(conv_event.text))
+
+
+if __name__ == '__main__':
+    run_example(receive_messages)
