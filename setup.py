049a966040fe85a8b94c7a331163ea6ad743f43b
Tom Dryer <tomdryer.com@gmail.com>
2020-09-06 22:05:10 -0700

    Add new Python versions

--- setup.py
+++ setup.py
@@ -57,6 +57,8 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Communications :: Chat',
         'Environment :: Console :: Curses',
     ],
