484d160a00eeeae74aa3f334dbe5a9767169cfba
Tom Dryer <tomdryer.com@gmail.com>
2020-09-27 18:41:31 -0700

    Upgrade pinned dependencies
    See #504.

--- setup.py
+++ setup.py
@@ -25,16 +25,16 @@ with open('README.rst') as f:
 # hangups from breaking when new versions of dependencies are released,
 # especially for end-users (non-developers) who use pip to install hangups.
 install_requires = [
-    'ConfigArgParse==0.11.0',
+    'ConfigArgParse>=0.11.0,<2',
     'aiohttp>=3.3,<4',
     'async-timeout>=2,<4',
     'appdirs>=1.4,<1.5',
-    'readlike==0.1.2',
+    'readlike>=0.1.2,<0.2',
     'requests>=2.6.0,<3',  # uses semantic versioning (after 2.6)
     'ReParser==1.4.3',
-    'protobuf>=3.1.0,<3.12',
-    'urwid==1.3.1',
-    'MechanicalSoup==0.6.0',
+    'protobuf>=3.1.0,<3.14',
+    'urwid>=1.3.1,<2.2',
+    'MechanicalSoup>=0.6.0,<0.13',
 ]
 
 
